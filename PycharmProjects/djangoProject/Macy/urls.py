0bf97def2559341134c710796389b1c363e6972a
taky95 <takumi.minohara08@gmail.com>
2020-12-30 21:53:16 -0700

    Fixed some bugs, added base pages and CSS design

--- PycharmProjects/djangoProject/Macy/urls.py
+++ PycharmProjects/djangoProject/Macy/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('signup/', views.SignupView.as_view(), name='signup'),
+	path('account_info/', views.AccountView.as_view(), name='account'),
 	path('', views.IndexView.as_view(), name='index_view'),
-	path()
 ]

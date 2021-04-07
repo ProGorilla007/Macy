78b65975c7ffe22aa0f3304bae9732b8f6197f1c
taky95 <takumi.minohara08@gmail.com>
2021-04-07 17:21:51 -0600

    removed Material libraries, and added OnsenUI

--- Macy/static/Macy/js/script.js
+++ Macy/static/Macy/js/script.js
@@ -1,12 +1,2 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, 900);
-
-    var dropdownOptions = {
-        'coverTrigger': false,
-        'autoTrigger': true,
-        'constrainWidth': false,
-    }
-    var dropdown = document.querySelectorAll('.dropdown-trigger');
-    var drop_down_menu = M.Dropdown.init(dropdown, dropdownOptions);
 });
